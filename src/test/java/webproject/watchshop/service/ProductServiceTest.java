5c38a240f5cb2355b5df054a0419a3ad321bd0fb
Ivaylo Ivanov <69511821+ivaka4@users.noreply.github.com>
2021-03-24 18:51:00 +0200

    Added tests

--- src/test/java/webproject/watchshop/service/ProductServiceTest.java
+++ src/test/java/webproject/watchshop/service/ProductServiceTest.java
@@ -1,10 +1,15 @@
 package webproject.watchshop.service;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,6 +18,8 @@ import org.springframework.web.multipart.MultipartFile;
 import webproject.watchshop.model.entity.Product;
 import webproject.watchshop.model.entity.ProductCategory;
 import webproject.watchshop.model.service.ProductServiceModel;
+import webproject.watchshop.model.view.ProductCategoryView;
+import webproject.watchshop.model.view.ProductViewModel;
 import webproject.watchshop.repository.ProductRepository;
 import webproject.watchshop.service.impl.ProductServiceImpl;
 
@@ -28,9 +35,10 @@ import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
-    @MockBean
+    @Mock
     private ProductRepository mockProductRepository;
 
     @InjectMocks
@@ -40,37 +48,51 @@ public class ProductServiceTest {
     private ModelMapper modelMapper;
 
     @Autowired
-    private  ProductCategoryService productCategoryService;
+    private ProductCategoryService productCategoryService;
 
     @Autowired
-    private  CloudinaryService cloudinaryService;
+    private CloudinaryService cloudinaryService;
 
     private Product product;
     private ProductServiceModel productServiceModel;
+    private ProductViewModel productViewModel;
 
     @BeforeEach
     public void setup() {
         //this.mockedStoreRepository = Mockito.mock(StoreRepository.class);
-       product = this.getProduct();
-       productServiceModel = this.getProductServiceModel();
-        this.productService = new ProductServiceImpl(this.mockProductRepository,
+        product = this.getProduct();
+        productViewModel = this.getProductViewModel();
+        productServiceModel = this.getProductServiceModel();
+        this.productService = new ProductServiceImpl(mockProductRepository,
                 this.modelMapper, this.productCategoryService, this.cloudinaryService);
 
     }
 
 
     @Test
-    public void testGetProductById() throws IOException {
-        when(mockProductRepository.findById(product.getId())).thenReturn(Optional.of(product));
+    public void testProductServiceGetAllProducts() {
+        when(productService.getAllProducts()).thenReturn(List.of(productServiceModel));
 
-        Product created = this.mockProductRepository.findById(1L).orElse(null);
-        System.out.println();
-        assert created != null;
-        Assert.assertEquals(product.getId(), created.getId());
+        List<ProductServiceModel> psm = productService.getAllProducts();
+        ProductServiceModel actualDto = psm.get(0);
+
+        Assertions.assertEquals(productServiceModel.getName(), actualDto.getName());
+        Assertions.assertEquals(productServiceModel.getProductNumber(), actualDto.getProductNumber());
 
     }
 
+    @Test
+    public void testProductServiceGetProductById(){
 
+        when(productService.getProductBy(1L)).thenReturn(productServiceModel);
+        System.out.println();
+        ProductServiceModel psvm = productService.getProductBy(1L);
+        System.out.println();
+        Assertions.assertEquals(productServiceModel.getId(), psvm.getId());
+        Assertions.assertEquals(productServiceModel.getName(), psvm.getName());
+
+
+    }
 
 
     public Product getProduct() {
@@ -94,7 +116,9 @@ public class ProductServiceTest {
         product.setCategory(productCategory);
 
         return product;
-    } public ProductServiceModel getProductServiceModel() {
+    }
+
+    public ProductServiceModel getProductServiceModel() {
         ProductServiceModel product = new ProductServiceModel();
         product.setId(1L);
         product.setEditedOn(LocalDateTime.now());
@@ -115,6 +139,29 @@ public class ProductServiceTest {
         productCategory.setCategory("Stylish");
         productCategory.setDescription("Stylish");
         product.setCategory("Stylish");
+
+        return product;
+    }
+
+    public ProductViewModel getProductViewModel() {
+        ProductViewModel product = new ProductViewModel();
+        product.setId(1L);
+        product.setEditedOn(LocalDateTime.now());
+        product.setAddedOn(LocalDateTime.now());
+        product.setPrice(BigDecimal.valueOf(250));
+        product.setImageUrls(
+                List.of("http://res.cloudinary.com/watch-shop-cloud/image/upload/v1616258871/qpy2zdazzvaawr8gucg8.jpg",
+                        "http://res.cloudinary.com/watch-shop-cloud/image/upload/v1616258874/hwnk9rhmoqstkbl36j4z.jpg",
+                        "http://res.cloudinary.com/watch-shop-cloud/image/upload/v1616258606/wiqyq3bgigdkskgymdyz.jpg"));
+        product.setModel("Casio");
+        product.setMake("Casio");
+        product.setProductNumber("12345");
+        product.setDescription("Product description");
+        product.setName("Rolex");
+        ProductCategoryView productCategory = new ProductCategoryView();
+        productCategory.setCategory("Stylish");
+        productCategory.setDescription("Stylish");
+        product.setCategory(productCategory);
 
         return product;
     }
