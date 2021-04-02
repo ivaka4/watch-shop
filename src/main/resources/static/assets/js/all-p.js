3be45d1a0e77879271a049fc56f383a50134c682
Ivaylo Ivanov <69511821+ivaka4@users.noreply.github.com>
2021-04-02 20:37:21 +0300

    Added shop sorting

---
+++ src/main/resources/static/assets/js/all-p.js
@@ -0,0 +1,52 @@
+const allP = document.getElementById('allProducts')
+
+const all = [];
+
+fetch("http://localhost:8080/shop/api").then(response => response.json()).then(data => {
+    for (let product of data) {
+        all.push(product);
+    }
+})
+
+allP.addEventListener('click', (e) => {
+    e.preventDefault();
+    displayAllProducts(all);
+})
+
+
+const displayAllProducts = (products) => {
+    productsList.innerHTML = products
+        .map((p) => {
+            return `
+
+                        <div 
+                              class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                            <div class="single-popular-items mb-50 text-center">
+                                <div class="popular-img">
+                                   <img src="${p.imageUrls[0]}" width="361" height="380"
+                                         alt="">
+                                    <div class="img-cap">
+                                        <a href="/add-to-cart/${p.id}"><span>Add to cart</span></a>
+                                        <a sec:authorize="hasAuthority('ADMIN')"
+                                           href="@{/product/edit/{id}(id = *{id})}"><span>Edit Product</span></a>
+                                        <a sec:authorize="hasAuthority('ADMIN')"
+                                           href="@{/product/delete/{id}(id = *{id})}"><span>Delete Product</span></a>
+                                    </div>
+                                    <div class="favorit-items">
+                                        <span class="flaticon-heart"></span>
+                                    </div>
+                                </div>
+                                <div class="popular-caption">
+                                    <h3><a href="/product/details/${p.id}">${p.name}</a>
+                                    </h3>
+                                    <span>${p.price}</span>
+                                </div>
+                            </div>
+                        </div>
+                       
+                    
+                 `
+        })
+        .join('');
+
+}
